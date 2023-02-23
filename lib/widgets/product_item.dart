bb2b54665b72167617933709245d76435985ead1
steadysamwise4 <davenportsam44@gmail.com>
2023-02-23 13:50:52 -0600

    added and began to implement provider package in product_item

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-  const ProductItem(this.id, this.title, this.imageUrl, {Key key})
-      : super(key: key);
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+  const ProductItem({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -18,23 +19,27 @@ class ProductItem extends StatelessWidget {
           onTap: (() {
             Navigator.of(context).pushNamed(
               ProductDetailScreen.routeName,
-              arguments: id,
+              arguments: product.id,
             );
           }),
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
+            icon: Icon(
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+            ),
+            onPressed: () {
+              product.toggleFavoriteStatus();
+            },
             color: Theme.of(context).colorScheme.secondary,
           ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
